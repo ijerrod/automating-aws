f3ae84a189972c704513a234f5ec07142d617af5
jerrod <jlwelsh@gmail.com>
2018-11-28 12:27:07 -0600

    sync directory tree to S3 bucket

--- s3-website/s3web/s3web.py
+++ s3-website/s3web/s3web.py
@@ -1,6 +1,8 @@
 import boto3
 import click
 from botocore.exceptions import ClientError
+from pathlib import Path
+import mimetypes
 
 session = boto3.Session(profile_name='pythonAutomation')
 s3 = session.resource('s3')
@@ -39,38 +41,63 @@ def setup_bucket(bucket):
         if e.response['Error']['Code'] == 'BucketAlreadyOwndedByYou':
             s3_bucket = s3.Bucket(bucket)
         else:
-            raise e:
+            raise e
 
-    policy = '''
+    policy = """
     {
-        "Version":"2012-10-17",
-        "Statement":[{
-	    "Sid":"PublicReadGetObject",
-        "Effect":"Allow",
-	    "Principal": "*",
-            "Action":["s3:GetObject"],
-            "Resource":["arn:aws:s3:::example-bucket/*"
-            ]
-          }
-       ]
-     }
-     ''' % s3_bucket.name
-     policy = policy.strip()
+      "Version":"2012-10-17",
+      "Statement":[{
+      "Sid":"PublicReadGetObject",
+      "Effect":"Allow",
+      "Principal": "*",
+          "Action":["s3:GetObject"],
+          "Resource":["arn:aws:s3:::example-bucket/*"
+          ]
+        }
+      ]
+    }
+    """ % s3_bucket.name
+    policy = policy.strip()
 
-     pol = s3_bucket.Policy()
-     pol.put(Policy=policy)
+    pol = s3_bucket.Policy()
+    pol.put(Policy=policy)
 
-     ws = s3_bucket.Website()
-     ws.put(WebsiteConfiguration={
+    ws = s3_bucket.Website()
+    ws.put(WebsiteConfiguration= {
         'ErrorDocument': {
-            'Key': 'error.html'
+          'Key': 'error.html'
         },
         'IndexDocument': {
-            'Suffix': 'index.html'
+          'Suffix': 'index.html'
         }
-     })
+    })
 
-     return
+    return
+
+def upload_file(s3_bucket, path, key):
+    content_type = mimetypes.guess_type(key)[0] or 'text/plain'
+    s3_bucket.upload_file(
+        path,
+        key,
+        ExtraArgs={
+            'ContentType': 'text/html'
+        })
+
+@cli.command('sync')
+@click.argument('pathname', type=click.Path(exists=True))
+@click.argument('bucket')
+def sync(pathname, bucket):
+    "Syncs contents of PATHNAME to Bucket"
+    s3_bucket = s3.Bucket(bucket)
+
+    root = Path(pathname).expanduser().resolve()
+
+    def handle_directory(target):
+        for p in target.iterdir():
+            if p.is_dir(): handle_directory(p)
+            if p.is_file(): upload_file(s3_bucket, str(p), str(p.relative_to(root)))
+
+    handle_directory(root)
 
 if __name__ == '__main__':
     cli()
