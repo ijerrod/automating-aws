21b380c80dbb0c48577a200516f073a5652abd0e
jerrod <jlwelsh@gmail.com>
2018-12-01 20:04:32 -0600

    Post messages to Slack - Not working

--- notify/handler.py
+++ notify/handler.py
@@ -1,9 +1,12 @@
-import json
+import os
 
+import requests
 
-def hello(event, context):
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    
+def post_to_slack(event, context):
+    slack_webhook_url = os.environ['SLACK_WEBHOOK_URL']
+
+    slack_message = "From {source} at {detail[StartTime]}: {detail[Description]}".format(**event)
+    data = { "text": slack_message }
+    requests.post(slack_webhook_url, json=data)
+
+    return
